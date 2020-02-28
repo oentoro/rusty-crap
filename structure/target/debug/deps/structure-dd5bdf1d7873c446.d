9b689799d809d7edf7d3585cf6261ecf2712a7b2
Caisar Oentoro <czaroentoro@gmail.com>
2020-02-28 15:52:30 +0700

    tambah structure

---
+++ structure/target/debug/deps/structure-dd5bdf1d7873c446.d
@@ -0,0 +1,5 @@
+/Users/finnet/ponia/rust/structure/target/debug/deps/structure-dd5bdf1d7873c446: src/main.rs
+
+/Users/finnet/ponia/rust/structure/target/debug/deps/structure-dd5bdf1d7873c446.d: src/main.rs
+
+src/main.rs:
